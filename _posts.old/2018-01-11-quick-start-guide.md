0cd22e29efcda16683486e0078c903d0be332cc3
Choi Taekyoon <tgchoi03@gmail.com>
2020-10-05 23:20:23 +0900

    updated the first post

--- _posts.old/2018-01-11-quick-start-guide.md
+++ _posts.old/2018-01-11-quick-start-guide.md
@@ -1,7 +1,7 @@
 ---
 layout: post
 title:  "Let's test spoilers"
-author: sal
+author: ted
 categories: [ Jekyll, tutorial ]
 image: assets/images/12.jpg
 featured: true
@@ -12,7 +12,7 @@ Director Roland Suso Richter's enigmatic psychological thriller (direct to video
 
 As in many similar films, such as Jacob's Ladder (1990), Soul Survivors (2001), and The Butterfly Effect (2004), events and people were thoroughly distorted and confused because the protagonist was at the point of death. The tagline was misleading:
 
-"When You Don't Have a Memory, How Can You Remember Who to Trust?"
+"When You Don't Have a Memory, How Can You Remember Who to Trust?
 
 The mind-warping film opened with a hospital patient Simon Cable (Ryan Phillippe) awakening in a <span class="spoiler"> hospital with little knowledge (amnesia perhaps?) of what had happened, and why he was there, etc. He was told by attending Dr. Jeremy Newman (Stephen Rea) that it was July 29, 2002 (Simon thought it was the year 2000 - he was confused - he heard a doctor say 20:00 hours!) and that he had died for two minutes from cardiac arrest following the near-fatal accident -- but he had been revived ("You're as good as new").</span> Dr. Newman: "Simon, this is the 29th of July. The year is 2002. And your wife, whose name is Anna, is waiting outside." 
 
---
+++ _site/assets/js/lunrsearchengine.js
@@ -0,0 +1,99 @@
+
+var documents = [{
+    "id": 0,
+    "url": "http://localhost:4000/404.html",
+    "title": "404",
+    "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
+    }, {
+    "id": 1,
+    "url": "http://localhost:4000/about",
+    "title": "Mediumish Template for Jekyll",
+    "body": "This website is built with Jekyll and Mediumish template for Jekyll. It's for demonstration purposes, no real content can be found. Mediumish template for Jekyll is compatible with Github pages, in fact even this demo is created with Github Pages and hosted with Github.  Documentation: Please, read the docs here. Questions or bug reports?: Head over to our Github repository! Buy me a coffeeThank you for your support! Your donation helps me to maintain and improve Mediumish . Buy me a coffee Documentation"
+    }, {
+    "id": 2,
+    "url": "http://localhost:4000/categories",
+    "title": "Categories",
+    "body": ""
+    }, {
+    "id": 3,
+    "url": "http://localhost:4000/",
+    "title": "Home",
+    "body": "      Featured:                                                                                                                                                                                                           시작                              :               무슨 글을 쓸까?:                                                                       27 Sep 2020                &lt;/span&gt;                                                                                                      All Stories:             "
+    }, {
+    "id": 4,
+    "url": "http://localhost:4000/robots.txt",
+    "title": "",
+    "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
+    }, {
+    "id": 5,
+    "url": "http://localhost:4000/intro/",
+    "title": "시작",
+    "body": "2020/09/27 - 무슨 글을 쓸까?: 블로그를 만들기 전 부터 기술에 대한 이야기를 풀어 써 보고 싶다는 생각은 들었습니다. 그런데 막상 글을 쓰려하면 뭔가 쉽게 써지지는 않았죠. 하고 싶은 이야기는 머리속에 많고 말은 어떻게 풀지 잘 몰랐으니까요. 그래도 글을 써보는 것이 낫겠다 싶어 이렇게 블로그 한 페이지에 뭔가 타이핑을 시작해 보고 있습니다. 제가 관심이 있는 내용들은 AI 기술들이고 그 중에서도 제가 가장 관심있어 하는 자연어 처리와 딥러닝 그리고 이 기술들을 아우르는 서비스에 대한 것들입니다. 저는 이 세 가지에 관한 주제를 다뤄보고자 합니다. 여기에 관련된 분들은 잘 아시겠지만 자연어 처리, 딥러닝, 그리고 서비스 이 세 부류 각각에 대해서는 잘 아실겁니다. 각 분야에 대한 지식 깊이도 상당히 깊고 대부분 한 분야에 대해서 집중하시기 때문입니다. 그런데 이 세 내용을 아울러서 이야기를 해보고 싶은데 그러기엔 아마도 제 사고의 흐름이 머지않아 꼬일 듯 싶습니다. 이 세 가지의 주제가 어렵기도 하고 섞어 이야기 했다가 무슨소리인지 이해하기 어렵기 때문입니다. 저는 이 각각의 분야에 대한 이야기를 하나씩 하나씩 풀어보려합니다. 하나 하나의 내용을 스토리로 이어 자연어 처리, 딥러닝, 서비스에 대한 내용을 독자분들에게 쉽게 이해할 수 있도록 하려 합니다. 자연어처리, 딥러닝, 그리고 서비스: 그런데 자연어처리, 딥러닝, 그리고 서비스 이 세 가지에 대한 이야기를 왜 할까요? 그리고 왜 제가 가장 관심있어 할까요?인공지능을 활용한 서비스 중 AI 비서가 우리 생활에 큰 변화를 줄 것이기 때문이고 이 서비스가 바로 자연어처리, 딥러닝, 서비스 3 요소가 반드시 필요하기 때문입니다. AI 비서는 지금으로부터 약 3~4년 전에서 부터 나왔던 이야기로 알고 있는데요. 과거에만 해도 광고에 나온 AI 비서 컨셉은 굉장히 현실적이지 않았던 경우가 많았습니다. 그런데 지금은 어떤가요? 어느정도 AI 비서가 단순한 일은 충분히 수행하는 듯 합니다. 비록 우리가 생각하고 기대했던 것 보다 긴 시간이 걸려 더 나아진 AI 서비스들을 볼 수 있었지만, 이를 가능하게 한 가장 큰 요인은 바로 이 자연어처리, 딥러닝의 기술의 발전입니다. 자연어처리 기술은 이미 1960년대에서 부터 지속적으로 연구를 해 온 분야 였지만 딥러닝 기술을 만나 괄목할 만한 성장을 이뤄온 분야입니다. 특히, Google과 Open AI에서 개발한 BERT와 GPT 기술은 현실적으로 인공지능이 똑똑해졌다라고 느낄 수 있는 수준까지 이르게 했습니다. 하지만, 기술 성장만이 AI 비서를 발전 시킬수 없습니다. AI 비서를 사용하는 유저들을 파악하고 제공할 수 있는 서비스 설계의 노하우 역시 필요합니다. 초기 AI 비서 서비스를 시작하는 사람들 입장에서는 모든 것이 다 멘땅에 헤딩이었을 겁니다. 하지만 지금은 나름 5년에 가까운 시간이 지났고 많은 인공지능을 다루는 기술 서비스들이 무르익었을 겁니다. 그래서, 엄청난 기술과 더불어 성숙한 서비스의 노하우가 지금의 괜찮은 AI 비서를 있도록 만들었습니다. 블로그에서는 AI 비서를 포함한 인공지능 서비스에 대한 최신 자연어처리, 딥러닝 기술과 더불어 성공적으로 인공지능 서비스로 이끈 사례까지 스토리를 풀어 이야기 합니다. 각 기술에 대한 내용을 하나하나 살피면서 서비스와 어떻게 연결을 지었는지 같이 살펴보았으면 합니다. 과거를 통해 AI 서비스의 미래를 알아보기: 이제 제가 이야기 할 거리가 무엇인지, 왜 이야기를 하는지 다들 아셨을 겁니다. 그런데 무엇을 하고 싶어서 이런 블로깅을 하는지에 대해서 아직 이야기 하지 않았습니다. 제가 블로그를 통해 하고 싶은 것은 과거의 AI 서비스를 통해 앞으로의 AI 서비스가 어떻게 변화할 수 있는지를 보는 것입니다. 독자분들이 사용하는 많은 서비스들은 수많은 실패와 경험을 통해서 지금의 서비스가 존재하고 있습니다. 과거에서부터 지금까지 어떤 기술과 서비스의 한계가 있었고 이들을 극복하기 위해 어떤 아이디어들이 등장했고 그리고 미래에 어떻게 활용할 수 있는지를 이야기하다보면 미래의 AI 서비스는 어떻게 될 수 있겠다는 예상을 할 수 있지 않을까요? 마치 인공지능처럼 말이죠. 제가 이야기 하는 스토리 끝에는 “앞으로 AI 서비스는 어떻게 될 수 있을까?” 라는 내용을 이야기 할 것입니다. 이 부분에 대해서 여러분과 같이 이야기를 나눠볼 수 있으면 좋을 것 같습니다. 이제부터 흥미진진한 이야기를 시작하도록 하겠습니다. "
+    }];
+
+var idx = lunr(function () {
+    this.ref('id')
+    this.field('title')
+    this.field('body')
+
+    documents.forEach(function (doc) {
+        this.add(doc)
+    }, this)
+});
+function lunr_search(term) {
+    document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
+    if(term) {
+        document.getElementById('lunrsearchresults').innerHTML = "<p>Search results for '" + term + "'</p>" + document.getElementById('lunrsearchresults').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>No results found...</li>";
+        }
+    }
+    return false;
+}
+
+function lunr_search(term) {
+    $('#lunrsearchresults').show( 400 );
+    $( "body" ).addClass( "modal-open" );
+    
+    document.getElementById('lunrsearchresults').innerHTML = '<div id="resultsmodal" class="modal fade show d-block"  tabindex="-1" role="dialog" aria-labelledby="resultsmodal"> <div class="modal-dialog shadow-lg" role="document"> <div class="modal-content"> <div class="modal-header" id="modtit"> <button type="button" class="close" id="btnx" data-dismiss="modal" aria-label="Close"> &times; </button> </div> <div class="modal-body"> <ul class="mb-0"> </ul>    </div> <div class="modal-footer"><button id="btnx" type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button></div></div> </div></div>';
+    if(term) {
+        document.getElementById('modtit').innerHTML = "<h5 class='modal-title'>Search results for '" + term + "'</h5>" + document.getElementById('modtit').innerHTML;
+        //put results on the screen.
+        var results = idx.search(term);
+        if(results.length>0){
+            //console.log(idx.search(term));
+            //if results
+            for (var i = 0; i < results.length; i++) {
+                // more statements
+                var ref = results[i]['ref'];
+                var url = documents[ref]['url'];
+                var title = documents[ref]['title'];
+                var body = documents[ref]['body'].substring(0,160)+'...';
+                document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML + "<li class='lunrsearchresult'><a href='" + url + "'><span class='title'>" + title + "</span><br /><small><span class='body'>"+ body +"</span><br /><span class='url'>"+ url +"</span></small></a></li>";
+            }
+        } else {
+            document.querySelectorAll('#lunrsearchresults ul')[0].innerHTML = "<li class='lunrsearchresult'>Sorry, no results found. Close & try a different search!</li>";
+        }
+    }
+    return false;
+}
+    
+$(function() {
+    $("#lunrsearchresults").on('click', '#btnx', function () {
+        $('#lunrsearchresults').hide( 5 );
+        $( "body" ).removeClass( "modal-open" );
+    });
+});
